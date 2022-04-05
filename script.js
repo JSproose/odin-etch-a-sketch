edd4f1726203816a83c406f53d34fba8fc779537
Jasper <jaspercalder@gmail.com>
2022-04-05 10:10:26 -0700

    Add color pixel darkening feature

--- script.js
+++ script.js
@@ -7,24 +7,26 @@ let dimensions = 16;
 
 
 function mouseHover(){
-    this.classList.add('hover')
+    backgroundColor = this.style.backgroundColor
+    if (((parseInt(backgroundColor.slice(4, 7))/250) - 0.1)*100 < 0) return;
+    const newValue = ((parseInt(backgroundColor.slice(4, 7))/250) - 0.1)*100
+    this.style.backgroundColor = `rgb(${newValue}%, ${newValue}%, ${newValue}%)`
 }
 
 function removeGrid() {
     const gridItems = document.querySelectorAll('.grid-item');
     gridItems.forEach(gridItem => {
-        // div_gridContainer.removeChild(gridItem);
         gridItem.remove();
     });
 }
 
 function createGrid() {
-    div_gridContainer.style.cssText = `width: 800px; height: 800px; background-color: #d3d3d3; display: inline-grid; 
-    grid-template-columns: repeat(${dimensions}, 1fr); border: 50px solid #bd162c; border-radius: 32px;`;
+    div_gridContainer.style.cssText = `width: 800px; height: 800px; display: inline-grid; 
+    grid-template-columns: repeat(${dimensions}, 1fr); border: 50px solid #bd162c; background-color: white; border-radius: 32px;`;
     for (i = 1; i <= (dimensions*dimensions); i++) {
         const div_gridItem = document.createElement('div');
         div_gridItem.classList.add('grid-item')
-        div_gridItem.style.cssText = 'height: auto;';
+        div_gridItem.style.cssText = 'height: auto; background-color: rgb(90%,90%,90%);';
         div_gridItem.addEventListener('mouseover', mouseHover);
         div_gridContainer.appendChild(div_gridItem);
     }
@@ -35,7 +37,6 @@ createGrid();
 
 button.addEventListener('click', () => {
     dimensions = parseInt(prompt('Chose the side lengths of the next grid: '));
-    console.log(dimensions*dimensions);
     removeGrid();
     createGrid();
 });
